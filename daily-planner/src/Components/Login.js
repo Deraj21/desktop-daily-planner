75eb11e3ff46d29539a27320c33c28a1d7e3941f
Jared Tanner <Deraj21@gmail.com>
2018-12-16 10:38:13 -0700

    login html

--- daily-planner/src/Components/Login.js
+++ daily-planner/src/Components/Login.js
@@ -5,7 +5,20 @@ export default class Login extends Component {
   render () {
     return (
       <div className="Login">
-        Login
+        <div className="title">
+          <h1>Daily Planner</h1>
+          </div>
+        <div className="login-box">
+          <div className="input-boxes">
+            <div>
+              <input type="text"/> <h3>Username</h3>
+            </div>
+            <div>
+              <input type="text"/> <h3>Password</h3>
+            </div>
+          </div>
+          <button><h1>Login</h1></button>
+        </div>
       </div>
     );
   }
